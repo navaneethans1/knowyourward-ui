4d7e7411d164078fa2f6e82a7c4d251034d98026
Arijit Bhattacharya <hoodwink73@live.com>
2018-03-25 21:39:13 +0530

    List all items for filter until user starts typing

--- src/components/Filter/index.js
+++ src/components/Filter/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Component from "@reactions/component";
 import Downshift from "downshift";
 import { Colors } from "@blueprintjs/core";
 import find from "lodash.find";
@@ -19,61 +20,73 @@ export default function({
     : null;
 
   return (
-    <Downshift
-      selectedItem={selectedItem}
-      onChange={onChange}
-      itemToString={itemToString}
-    >
-      {({
-        getInputProps,
-        getItemProps,
-        isOpen,
-        openMenu,
-        inputValue,
-        highlightedIndex
-      }) => {
-        return (
-          <div>
-            <input
-              className="pt-input pt-large pt-fill"
-              {...getInputProps({
-                dir: "auto",
-                type: "text",
-                placeholder,
-                onFocus: openMenu,
-                isOpen,
-                disabled
-              })}
-            />
-            {isOpen ? (
-              <ul className="pt-menu pt-large pt-elevation-0">
-                {items &&
-                  items
-                    .filter(i =>
-                      renderItemLabel(i)
-                        .toLowerCase()
-                        .includes(inputValue.toLowerCase())
-                    )
-                    .map((item, index) => (
-                      <li {...getItemProps({ item })} key={index}>
-                        <a
-                          className="pt-menu-item"
-                          style={{
-                            backgroundColor:
-                              highlightedIndex === index
-                                ? Colors.LIGHT_GRAY4
-                                : Colors.WHITE
-                          }}
-                        >
-                          {renderItemLabel(item)}
-                        </a>
-                      </li>
-                    ))}
-              </ul>
-            ) : null}
-          </div>
-        );
+    <Component
+      initialState={{ isDirty: false }}
+      didUpdate={({ setState }) => {
+        isDirty: false;
       }}
-    </Downshift>
+    >
+      {({ state, setState }) => (
+        <Downshift
+          selectedItem={selectedItem}
+          onChange={onChange}
+          itemToString={itemToString}
+        >
+          {({
+            getInputProps,
+            getItemProps,
+            isOpen,
+            openMenu,
+            inputValue,
+            highlightedIndex
+          }) => {
+            return (
+              <div>
+                <input
+                  className="pt-input pt-large pt-fill"
+                  {...getInputProps({
+                    dir: "auto",
+                    type: "text",
+                    placeholder,
+                    onFocus: openMenu,
+                    onKeyDown: () =>
+                      !state.isDirty && setState({ isDirty: true }),
+                    disabled
+                  })}
+                />
+                {isOpen ? (
+                  <ul className="pt-menu pt-large pt-elevation-0">
+                    {items &&
+                      items
+                        .filter(
+                          i =>
+                            !state.isDirty ||
+                            renderItemLabel(i)
+                              .toLowerCase()
+                              .includes(inputValue.toLowerCase())
+                        )
+                        .map((item, index) => (
+                          <li {...getItemProps({ item })} key={index}>
+                            <a
+                              className="pt-menu-item"
+                              style={{
+                                backgroundColor:
+                                  highlightedIndex === index
+                                    ? Colors.LIGHT_GRAY4
+                                    : Colors.WHITE
+                              }}
+                            >
+                              {renderItemLabel(item)}
+                            </a>
+                          </li>
+                        ))}
+                  </ul>
+                ) : null}
+              </div>
+            );
+          }}
+        </Downshift>
+      )}
+    </Component>
   );
 }
